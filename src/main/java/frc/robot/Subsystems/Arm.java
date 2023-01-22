85810d50f2b27a0d3a23f6ad5e0a6e5aefe5d600
Shai Grossman <72968565+korozya-bit@users.noreply.github.com>
2023-01-23 00:30:13 +0200

    Update src/main/java/frc/robot/Subsystems/Arm.java

--- src/main/java/frc/robot/Subsystems/Arm.java
+++ src/main/java/frc/robot/Subsystems/Arm.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-
 import java.util.function.DoubleSupplier;
 
 import static frc.robot.Constants.ArmConstants.*;
