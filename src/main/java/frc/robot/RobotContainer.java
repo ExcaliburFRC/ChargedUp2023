06418be70705e2abe0c208111712a4bacd35d511
excalibur <ExcaliburFRC@gmail.com>
2023-02-16 12:53:08 +0200

    fixed robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.Arm;
-import frc.robot.utiliy.Superstructure;
+import frc.robot.subsystems.Superstructure;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
