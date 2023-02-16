b6d98e7182af23916dacafa68e4cd5ae9df9d52a
excalibur <ExcaliburFRC@gmail.com>
2023-02-16 12:42:45 +0200

    switched constants to enum based

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,25 +1,42 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
+
+import java.util.PrimitiveIterator;
 
 import static frc.robot.Constants.ClawConstants.*;
+import static frc.robot.Constants.IntakeConstants.DISTANCE_THRESHOLD;
+import static frc.robot.Constants.IntakeConstants.GAME_PIECE_THRESHOLD;
 
 public class Claw extends SubsystemBase {
   private final DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.REVPH, FORWARD_CHANNEL, REVERSE_CHANNEL);
 
   private final DigitalInput beambreak = new DigitalInput(BEAMBREAK_CHANNEL);
   private final DigitalInput button = new DigitalInput(BUTTON_CHANNEL);
-
+private final ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kMXP);
   private final Trigger isClawOpenedTrigger = new Trigger(() -> piston.get().equals(DoubleSolenoid.Value.kReverse));
   private final Trigger beambreakDetectedTrigger = new Trigger(() -> !beambreak.get());
   private final Trigger clawFullTrigger = new Trigger(() -> !button.get());
+//  private final Trigger ColorSensorTrigger = new Trigger(()-> !colorSensor.get());
 
   public Claw() {
+  }
+
+
+  public GamePiece getCurrentItem() {
+    if (colorSensor.getProximity() < DISTANCE_THRESHOLD) {
+      if (colorSensor.getBlue() < GAME_PIECE_THRESHOLD) return GamePiece.CONE;
+      return GamePiece.CUBE;
+    }
+    return GamePiece.EMPTY;
   }
 
   public Command autoClawCommand() {
