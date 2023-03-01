c252a900d6c474369ee794ccea31afea40212834
Excalibur FRC | 6738 <excaliburfrc@gmail.com>
2023-03-01 19:26:06 +0200

    add fault reporter

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.utility.FaultReporter;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -22,6 +23,10 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  public Robot(){
+    addPeriodic(new FaultReporter()::check, 1);
+  }
 
   /**
    * This function is run when the robot is first started up and should be used for any
