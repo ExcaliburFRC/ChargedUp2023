0a25fa844e1a171e4a9dbbfeaf7aa76522cf48e9
Excalibur <excaliburfrc@gmail.com>
2023-03-27 13:08:41 +0300

    continued work on leds

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -5,12 +5,12 @@ import static frc.robot.Constants.LedsConstants.LEDS_PORT;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.utility.Color;
 
 public class LEDs extends SubsystemBase {
-    private final AddressableLED addressableLEDs = new AddressableLED(LEDS_PORT);
-    private final AddressableLEDBuffer leds = new AddressableLEDBuffer(LEDS_LENGTH);
+    private final AddressableLED leds = new AddressableLED(LEDS_PORT);
+    private final AddressableLEDBuffer buffer = new AddressableLEDBuffer(LEDS_LENGTH);
 
     private double brightness = 1;
 
@@ -23,22 +23,21 @@ public class LEDs extends SubsystemBase {
     }
 
     public LEDs() {
-        addressableLEDs.setData(leds);
-        addressableLEDs.setLength(LEDS_LENGTH);
-        addressableLEDs.start();
+        leds.setData(buffer);
+        leds.setLength(LEDS_LENGTH);
+        leds.start();
     }
 
-
-
-    void setLedColor(Color[] colors){
+    private void setLedColor(Color[] colors){
         for (int i = 0; i < colors.length; i++) {
-            leds.setLED(i, colors[i]);
+            buffer.setLED(i, Color.balance(colors[i]));
         }
+        leds.setData(buffer);
     }
 
     Color applyBrightness(double brightness, Color color){
         this.brightness = brightness;
-        return new Color(color.red * this.brightness, color.green * brightness, color.blue * brightness);
+        return new Color(color.red * this.brightness, color.green * this.brightness, color.blue * this.brightness);
     }
 
     Color applyBrightness(Color color){
