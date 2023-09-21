ed0b5582bcc02254de95b80f16932988ae372b0b
excalibur <ExcaliburFRC@gmail.com>
2023-09-22 01:34:35 +0300

    update roller gripper back to a sparkMax

--- src/main/java/frc/robot/subsystems/Rollergripper.java
+++ src/main/java/frc/robot/subsystems/Rollergripper.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
@@ -15,30 +14,47 @@ import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
 import static frc.robot.Constants.RollerGripperConstants.*;
 
 public class Rollergripper extends SubsystemBase {
-    private final CANSparkMax rightRoller = new CANSparkMax(RIGHT_ROLLER_MOTOR_ID, kBrushless);
-
-    // This is very embarrassing and not at all ideal, please don't laugh at us,
-    // There is a world shortout for SparkMax's, and we had those lying around.
-    private final Spark leftRoller = new Spark(LEFT_ROLLER_MOTOR_ID);
+    private final CANSparkMax rollers = new CANSparkMax(LEFT_ROLLER_MOTOR_ID, kBrushless);
+    private final CANSparkMax follower = new CANSparkMax(RIGHT_ROLLER_MOTOR_ID, kBrushless);
 
     private final DigitalInput beambreak = new DigitalInput(BEAMBREAK_PORT);
-
-    public final Trigger beambreakTrigger = new Trigger(() -> !beambreak.get()).debounce(0.1);
+    public final Trigger beambreakTrigger = new Trigger(() -> !beambreak.get());
 
     public Rollergripper() {
-        rightRoller.restoreFactoryDefaults();
-        rightRoller.clearFaults();
-        rightRoller.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        follower.restoreFactoryDefaults();
+        follower.clearFaults();
+        follower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+//        follower.follow(rollers, true);
 
-        leftRoller.setInverted(true);
-        rightRoller.setInverted(true);
+        rollers.restoreFactoryDefaults();
+        rollers.clearFaults();
+        rollers.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        rollers.setInverted(false);
 
         Arm.armTab.addBoolean("isConeDetected", beambreakTrigger)
                 .withPosition(10, 4).withSize(4, 2);
         Limelight.armCameraTab.addBoolean("isConeDetected", beambreakTrigger)
                 .withSize(2, 8);
 
-        setDefaultCommand(holdConeCommand().repeatedly());
+        setDefaultCommand(setRollerGripperMotor(0.25));
+    }
+
+    /**
+     * sets the roller gripper motors speeds
+     *
+     * @param speed the speed the motors spin at
+     * @return the command
+     */
+    private Command setRollerGripperMotor(double speed) {
+        return Commands.runEnd(() -> {
+                    rollers.set(speed);
+                    follower.set(speed);
+                },
+                () -> {
+                    rollers.stopMotor();
+                    follower.stopMotor();
+                },
+                this);
     }
 
     /**
@@ -52,13 +68,11 @@ public class Rollergripper extends SubsystemBase {
     public Command intakeCommand() {
         return Commands.runEnd(
                         () -> {
-                            rightRoller.set(0.25);
-                            leftRoller.set(0.35);
+                            rollers.set(0.75);
                             Shuffleboard.selectTab("armCamera");
                         },
                         () -> {
-                            rightRoller.stopMotor();
-                            leftRoller.stopMotor();
+                            rollers.stopMotor();
                             Shuffleboard.selectTab("driveTab");
                         },
                         this)
@@ -69,21 +83,15 @@ public class Rollergripper extends SubsystemBase {
      * ejectCommand
      * <p><b> noInit </b>starts spinning the motors outwards<br>
      * <b>noEnd </b>stops the motors</p>
-     * ends when the button senses that a cone is no longer in the roller gripper
+     * ends when the button senses that a cone is not longer in the roller gripper
      *
      * @return the command
      */
     public Command ejectCommand(double offset) {
         return Commands.runEnd(
-                        () -> {
-                            rightRoller.set(-0.025 - offset);
-                            leftRoller.set(-0.025 - offset);
-                        },
-                        () -> {
-                            rightRoller.stopMotor();
-                            leftRoller.stopMotor();
-                        },
-                        this)
+                        () -> rollers.set(-0.025 - offset),
+                        rollers::stopMotor,
+                        this) //runEnd ends here
                 .until(beambreakTrigger.negate().debounce(0.2));
     }
 
@@ -92,71 +100,15 @@ public class Rollergripper extends SubsystemBase {
     }
 
     /**
-     * waits until the driver wants to release the cone and then returns the eject command
-     *
-     * @param release the button that needs to be pressed in order to eject
-     * @return the eject command after the button was pressed
-     */
-    public Command releaseCommand(BooleanSupplier release) {
-        return new RunCommand(() -> {
-        }).until(release).andThen(ejectCommand());
-    }
-
-    /**
-     * manual command the allows full manual control of the system
-     *
-     * @param intake  whether the system should currently intake
-     * @param outtake whether the system should currently eject
-     * @return the command
-     */
-    public Command manualCommand(Trigger intake, Trigger outtake) {
-        return Commands.runEnd(
-                () -> {
-                    if (intake.getAsBoolean() || outtake.getAsBoolean()) {
-                        if (intake.getAsBoolean()) {
-                            rightRoller.set(0.6);
-                            leftRoller.set(0.6);
-                        }
-                        if (outtake.getAsBoolean()) {
-                            rightRoller.set(-0.1);
-                            leftRoller.set(-0.1);
-                        }
-                    } else {
-                        rightRoller.set(0);
-                        leftRoller.set(0);
-                    }
-                },
-                () -> {
-                    rightRoller.set(0);
-                    leftRoller.set(0);
-                },
-                this
-        );
-    }
-
-    /**
      * applies a small force to the roller gripper in order to hold it in place
      *
      * @return the command
      */
-//    private Command holdConeCommand(){
-//        return new ConditionalCommand(
-//                new InstantCommand(()-> {
-//                    rightRoller.set(0.1);
-//                    leftRoller.set(0.15);
-//                }, this),
-//                new InstantCommand(()-> {
-//                    rightRoller.stopMotor();
-//                    leftRoller.stopMotor();
-//                }, this),
-//                beambreakTrigger
-//        );
-//    }
-
-    private Command holdConeCommand(){
+    private Command holdConeCommand() {
         return new ConditionalCommand(
-                new InstantCommand(()-> rightRoller.set(0.05), this),
-                new InstantCommand(rightRoller::stopMotor, this),
+                Commands.runEnd(() -> rollers.set(0.25), rollers::stopMotor, this).withTimeout(0.05),
+                new InstantCommand(() -> {
+                }, this),
                 beambreakTrigger
         );
     }
